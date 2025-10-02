77e9dd8dfae0b3a9e8e01f5301484d93adb4c3d8
yogu116 <yogeshbhawal116@gmail.com>
2025-10-02 15:37:02 +0530

    Create index.js

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const authRoutes = require('./routes/auth');
+const taskRoutes = require('./routes/tasks');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => console.log('MongoDB connected'));
+
+app.use('/api/auth', authRoutes);
+app.use('/api/tasks', taskRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
